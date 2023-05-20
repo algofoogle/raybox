14efc736890b36043a6af22c05d5a82860536a48
Anton Maurovic <anton@foogle.com>
2023-05-20 17:13:07 +0930

    Bit more tracer FSM cleanup. Down to 4 states now

--- src/rtl/tracer.v
+++ src/rtl/tracer.v
@@ -58,8 +58,7 @@ module tracer(
     output reg          store,              // Driven high when we've got a result to store.
     output      [9:0]   column,             // The column we'll write to in the trace_buffer.
     output reg          side,               // The side data we'll write for the respective column.
-    output      [15:0]  vdist,
-    // output      [7:0]   height,             // The height data we'll write for the column. NOTE: Make sure we only output 1..240
+    output      [15:0]  vdist,							// Distance this column is from the viewer.
     output      [5:0]   tex,                // X coordinate (column) of wall's texture where the hit occurred.
 
     // Map ROM access:
@@ -68,13 +67,12 @@ module tracer(
     input       [1:0]   map_val
 );
 
-    localparam INIT     = 0;
+    localparam PREP     = 0;
     localparam STEP     = 1;
-    localparam CHECK    = 2;
-    localparam NEXT     = 3;  // For now, this has a 1 suffix because there are other repeats of this...
-    localparam STOP     = 4;
-
-    reg [3:0] state;
+    localparam TEST     = 2;
+    localparam DONE     = 3;  // For now, this has a 1 suffix because there are other repeats of this...
+    
+    reg [1:0] state;
     reg hit;
     reg [9:0] col_counter;
 
@@ -164,30 +162,6 @@ module tracer(
     //HACK: Range [6:-9] are enough bits to get the precision and limits we want for distance,
     // i.e. UQ7.9 allows distance to have 1/512 precision and range of [0,127).
 
-    // wire `F     heightScale;
-
-    // // Use a wall reference height of 256, which makes the maths simpler
-    // // (i.e. simple bit extraction instead of a multiplier), and happens to
-    // // also make the aspect ratio closer to square for each map cell:
-    // wire [7:0] wallHeight =
-    //     (heightScale >= `intF(1)) ? 8'd255 : // Cap height at 255 if heightScale >= 1.0
-    //     heightScale[-1:-8];  // Else we just need the upper 8 bits of the fractional part.
-    // //SMELL: Yes, this is hard-coded, but it works given we are assuming a max height of 255
-    // // (so, a full 8-bit range), and hence the upper 8 bits of precision would effectively
-    // // be multiplied by 256 anyway. Example:
-    // // - If heightScale is 1.0 or 1.5, then the first condition catches it and clamps it to 255.
-    // // - If heightScale is 0.75, then the second condition should yield 256*0.75=192.
-    // //   It accomplishes this by just grabbing the upper 8 fractional bits: b.1100'0000
-    // //   which is 192.
-    // //NOTE: First (cap) condition could probably just be: |`FI(heightScale).
-
-    // // Determine the final height value we'll write:
-    // assign height =
-    //     // Write 0 if we're in the "dead" region. //SMELL: We don't want this in future.
-    //     (state == LCLEAR || state == RCLEAR) ? 8'd0 :
-    //     // Values above 240 are CURRENTLY too high, so clamp:
-    //     (wallHeight > 8'd240) ? 8'd240 :
-    //     wallHeight;
     // Output current column counter value:
     assign column = col_counter;
 
@@ -215,12 +189,12 @@ module tracer(
             rayAddY <= -vplaneY<<<8;
 
             side <= 0;
-            state <= INIT;
+            state <= PREP;
         end else begin
             trace_cycle_count = trace_cycle_count + 1; //DEBUG
             // We must be enabled (and not in reset) so we're a free-running system now...
             case (state)
-                INIT: begin
+                PREP: begin
                     // Get the cell the player's currently in:
                     mapX <= playerXint;
                     mapY <= playerYint;
@@ -231,8 +205,6 @@ module tracer(
                 end
                 STEP: begin
                     //SMELL: Can we explicitly set different states to match which trace/step we're doing?
-                    // Might be easier to read than this muck.
-                    // if (satY || trackXdist < trackYdist) begin
                     if (needStepX) begin
                         mapX <= rxi ? mapX+1'b1 : mapX-1'b1;
                         trackXdist <= trackXdist + stepXdist;
@@ -242,35 +214,34 @@ module tracer(
                         trackYdist <= trackYdist + stepYdist;
                         side <= 1;
                     end
-                    state <= CHECK;
+                    state <= TEST;
                 end
-                CHECK: begin
+                TEST: begin
                     // Check if we've hit a wall yet.
                     if (map_val!=0) begin
                         // Hit a wall.
-                        state <= NEXT; // Finish the column; advance to next or stop.
+                        state <= DONE; // Finish the column; advance to next or stop.
 												store <= 1;
                     end else begin
                         // No hit yet; keep going.
                         state <= STEP;
                     end
                 end
-                NEXT: begin
+                DONE: begin
                     // Store is finished.
                     store <= 0;
                     if (col_counter == 575) begin
-                        // No more columns to trace.
-                        state <= STOP;
+                        // No more columns to trace; hang here.
+                        state <= DONE;
                     end else begin
                         // Start the next column.
                         col_counter <= col_counter + 1'b1;
                         // Advance the ray dir offset (rayAdd) by 1 whole vplane vector value:
                         rayAddX <= rayAddX + vplaneX;
                         rayAddY <= rayAddY + vplaneY;
-                        state <= INIT;
+                        state <= PREP;
                     end
                 end
-								// If none of the above, must be STOP.
             endcase
         end
     end
