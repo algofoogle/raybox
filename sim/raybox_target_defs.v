e7e8c1d7628bc89a383515ffef8e1c3a4bc9e597
algofoogle <anton@foogle.com>
2023-05-23 09:55:07 +0200

    Oops, forgot simraybox_target_defs.v

---
+++ sim/raybox_target_defs.v
@@ -0,0 +1,10 @@
+// This file is included by some of the main RTL files, and some targets require
+// this for global definitions (e.g. Quartus).
+
+//NOTE: This file has the same name as another used by other targets, to hopefully
+// allow for generic `include statements that will end up picking the correct file
+// by virtue of the compiler used.
+
+`default_nettype none
+`timescale 1ns / 1ps
+
